f887638023651bda63c2c62a277a37a62448ae5b
José Roberto de Oliveira Júnior <jroberto.financas@gmail.com>
2026-07-22 16:20:56 -0300

    Criado a controller aula 2

---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.Java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nessa rota!!!";
+    }
+
+}
